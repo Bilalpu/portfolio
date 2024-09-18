7dcf4593dde15ff50e4354b7a3637e000438df38
Bilalpu <bilalqureshi752b@gmail.com>
2024-09-18 13:08:13 +0500

    index.html

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('toggleButton').addEventListener('change', function() {
+  if(this.checked) {
+    // Code for the 'on' state
+    console.log('Toggle is ON');
+  } else {
+    // Code for the 'off' state
+    console.log('Toggle is OFF');
+  }
+});
